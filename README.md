b72d888bf33b06f540459e760f792f6cb2403963
Andre Torrinha <am.torrinha@campus.fct.unl.pt>
2023-10-27 20:09:51 +0100

    Add remaining game logic and styling

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# odin-tic-tac-toe
+# Tic Tac Toe
+Small project that consists of a page with a Tic Tac Toe board and a simple menu for user interaction.
+
+With this work, I mainly practiced some of JavaScript's design patterns for encapsulating functionality, like factory functions and modules.
--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-function player (number, symbol) {
-    const name = "Player" + number;
-    return { name, number, symbol };
+function player (number, name, symbol) {
+    return { number, name, symbol };
 }
 
 const gameBoard = (function () {
@@ -8,36 +7,52 @@ const gameBoard = (function () {
     const EMPTY = " ";
     const X_FILL = "X";
     const O_FILL = "O";
-    boardState = Array(BOARD_SIZE).fill(EMPTY);
+
+    let boardState = Array(BOARD_SIZE).fill(EMPTY);
 
     const playRound = (index, symbol) => {
         cellState = boardState[index];
-        console.log("CELL STATE "+ cellState);
-        console.log(boardState.length);
-        console.log(boardState);
         if (cellState !== EMPTY) return; // cell already used
         else {
-            console.log("IM PLAYING");
-            console.log(symbol);
             boardState[index] = symbol;
-            console.log(index);
-            console.log(boardState[index]);
-            return index; // return for validation 
+            return symbol; // return for validation 
         }
+    }
+
+    const getCellState = (index=undefined) => {
+        if(index !== undefined) return boardState[index];
+        else return boardState;
+    }
+
+    const isFinished = () => {
+        const winPatterns = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
+            [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
+            [0, 4, 8], [2, 4, 6]             // Diagonals
+        ];
+        for (const pattern of winPatterns) {
+            const [a, b, c] = pattern;
+            if (boardState[a] === EMPTY) continue;
+            if (boardState[a] && boardState[a] === boardState[b] && boardState[a] === boardState[c]) {
+                return boardState[a];
+            }
+        }
+        if (!boardState.includes(EMPTY)) return EMPTY; // It's a tie!
     }
 
     const resetBoard = () => {
         boardState = Array(BOARD_SIZE).fill(EMPTY);
     }
-    return { boardState, playRound, resetBoard };
+
+    return { EMPTY, X_FILL, O_FILL, getCellState, playRound, isFinished, resetBoard };
 })();
 
 const game = (function() {
-    player1 = player(1, gameBoard.X_FILL);
-    player2 = player(2, gameBoard.O_FILL);
-    currentPlayer = player1;
+    let player1;
+    let player2;
+    let currentPlayer = player1;
+    let isActive = false;
 
-    gameBoard.resetBoard();
     const cells = document.querySelectorAll(".cell");
     // Add Click Interaction
     cells.forEach(function(cell, index) {
@@ -46,32 +61,65 @@ const game = (function() {
         });
     })
 
+    const form = document.getElementById('myForm');
+    form.addEventListener("submit", function(event) {
+        event.preventDefault(); // Prevent the default form submission
+        initGame();
+        
+        var elements = form.elements;
+        // Disable each form element
+        for (var i = 0; i < elements.length; i++) {
+            elements[i].disabled = true;
+        }
+    });
+
+    const resetBtn = document.querySelector(".reset-btn");
+    resetBtn.addEventListener("click", function() {
+        resetGame();
+    });
+
     const initGame = () => {
-        //above code was inside here b4
+        gameBoard.resetBoard();
+        name1 = document.getElementById('player1').value;
+        name2 = document.getElementById('player2').value;
+        player1 = player(1, name1, gameBoard.X_FILL);
+        player2 = player(2, name2, gameBoard.O_FILL);
+        currentPlayer = player1;
+        isActive = true;
     };
 
     const playRound = (index) => {
-        console.log("PLAYING ROUND " + index);
-        console.log(currentPlayer);
+        if (!isActive) return;
         if(gameBoard.playRound(index, currentPlayer.symbol)) {
-            currentPlayer = currentPlayer === player1 ? player2 : player1; // switch player
-            // check game end logic
             displayGame();
+            // Checks game end
+            isFinished = gameBoard.isFinished();
+            if (isFinished) {
+                if (isFinished === gameBoard.EMPTY) endGame(true);
+                else endGame();
+            }
+            currentPlayer = (currentPlayer === player1) ? player2 : player1; // switch player
         };
     }
 
     const displayGame = () => {
         cells.forEach(function(cell, index) {
-            cell.innerHTML = gameBoard.boardState[index];
+            cell.innerHTML = gameBoard.getCellState(index);
         });
     }
 
-    const playGame = () => {
-
+    const endGame = (tie = false) => {
+        if (tie) alert("It's a tie!");
+        else alert(`Game Finished. ${currentPlayer.name} wins!`);
+        setTimeout(function() { resetGame(); }, 1000);
     }
-    return { initGame, displayGame, playGame };
+
+    const resetGame = () => {
+        currentPlayer = player1;
+        gameBoard.resetBoard();
+        displayGame();
+    }
+
+    return { initGame, displayGame};
 })();
 
-game.initGame();
-game.displayGame();
-// game.playGame();
